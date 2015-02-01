0ddea2486e3623a95c867ce242ec80a77ff70580
Jack Chatterton <chattj@gmail.com>
2015-01-31 17:00:08 -0800

    initial commit

---
+++ VideoPulse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPulse
+//
+//  Created by Jack Chatterton on 1/31/15.
+//  Copyright (c) 2015 Postreal Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
