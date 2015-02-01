9d13a358d0429dcb85e0b36c673a34e5bd05ba55
Jack Chatterton <chattj@gmail.com>
2015-01-31 17:23:22 -0800

    add video playback button & action

--- VideoPulse/ViewController.m
+++ VideoPulse/ViewController.m
@@ -16,12 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+-(IBAction) playVideo:(id) sender {
+    NSURL *MyURL = [[NSBundle mainBundle] URLForResource: @"video" withExtension:@"mov"];
+    NSLog(@"Got one %@", MyURL);
+}
+
 
 @end
