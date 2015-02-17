84347292a01cdc09b7edce33e1b5870545a92b90
Jack Chatterton <chattj@gmail.com>
2015-02-16 19:43:57 -0800

    use eaglcontext for ciimage processing

--- VideoPulse/VPStreamProcessor.m
+++ VideoPulse/VPStreamProcessor.m
@@ -10,12 +10,34 @@
 
 @implementation VPStreamProcessor {
     CGImageRef lastImage;
+    CIDetector *detector;
+    CIContext *ciContext;
+    CIFilter *radialFilter;
+    CIFilter *maskFilter;
+}
+
+-(id)init {
+    if ( self = [super init] ) {
+        EAGLContext *eagl = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+        ciContext = [CIContext contextWithEAGLContext:eagl];
+        [EAGLContext setCurrentContext:eagl]; // todo: this may not be doing anything useful
+        detector =[CIDetector detectorOfType:CIDetectorTypeFace
+                                     context:ciContext
+                                     options:nil];
+
+        radialFilter = [CIFilter filterWithName:@"CIRadialGradient"];
+        [radialFilter setDefaults];
+        CIColor *alphaOne = [CIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+        CIColor *alphaZero = [CIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0];
+        [radialFilter setValue:alphaOne forKey:@"inputColor0"];
+        [radialFilter setValue:alphaZero forKey:@"inputColor1"];
+
+        maskFilter = [CIFilter filterWithName:@"CISourceInCompositing"];
+    }
+    return self;
 }
 
 - (CGImageRef)getFaceFromFrame:(CIImage *)ciImage {
-    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
-                                            context:nil
-                                            options:nil];
     NSArray *faceArray = [detector featuresInImage:ciImage options:nil];
 
     if (faceArray.count > 0) {
@@ -24,25 +46,17 @@
         CGFloat yCenter = face.bounds.origin.y + face.bounds.size.height/2.0;
         CIVector *center = [CIVector vectorWithX:xCenter Y:yCenter];
         CGFloat radius = face.bounds.size.width/2.8;
-        CIColor *alphaOne = [CIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
-        CIColor *alphaZero = [CIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0];
 
         NSLog(@"face at %f, %f with radius %f", xCenter, yCenter, radius);
 
-        CIFilter* filter = [CIFilter filterWithName:@"CIRadialGradient"];
-        [filter setDefaults];
-        [filter setValue:center forKey:@"inputCenter"];
-        [filter setValue:[NSNumber numberWithFloat:radius] forKey:@"inputRadius0"];
-        [filter setValue:[NSNumber numberWithFloat:radius] forKey:@"inputRadius1"];
-        [filter setValue:alphaOne forKey:@"inputColor0"];
-        [filter setValue:alphaZero forKey:@"inputColor1"];
+        [radialFilter setValue:center forKey:@"inputCenter"];
+        [radialFilter setValue:[NSNumber numberWithFloat:radius] forKey:@"inputRadius0"];
+        [radialFilter setValue:[NSNumber numberWithFloat:radius] forKey:@"inputRadius1"];
 
-        CIFilter *maskFilter = [CIFilter filterWithName:@"CISourceInCompositing"
-                                          keysAndValues:
-                                @"inputImage", ciImage,
-                                @"inputBackgroundImage", [filter outputImage], nil];
+        [maskFilter setValue:ciImage forKey:@"inputImage"];
+        [maskFilter setValue:[radialFilter outputImage] forKey:@"inputBackgroundImage"];
 
-        return [[CIContext contextWithOptions:nil] createCGImage:[maskFilter outputImage]
+        return [ciContext createCGImage:[maskFilter outputImage]
                                fromRect:[ciImage extent]];
     } else {
         NSLog(@"no face detected");
