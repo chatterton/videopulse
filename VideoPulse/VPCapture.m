1236753bd54f84d866ca002cf371a088e6df4996
Jack Chatterton <chattj@gmail.com>
2015-02-21 17:06:32 -0800

    set image on main thread; stops dealloc crash

--- VideoPulse/VPCapture.m
+++ VideoPulse/VPCapture.m
@@ -73,34 +73,28 @@
 }
 
 -(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
-
-    CGImageRef newImage = [self imageFromSampleBuffer:sampleBuffer];
-    if (newImage) {
-        CGImageRelease(self.lastCapturedImage);
-        self.lastCapturedImage = newImage;
-    }
-
+    [self lastImageFromSampleBuffer:sampleBuffer];
 }
 
-// Create an image from sample buffer data
-- (CGImageRef) imageFromSampleBuffer:(CMSampleBufferRef) sampleBuffer {
+// Set last image from sample buffer data
+- (void) lastImageFromSampleBuffer:(CMSampleBufferRef) sampleBuffer {
     // Get a CMSampleBuffer's Core Video image buffer for the media data
     CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
     // Lock the base address of the pixel buffer
     CVPixelBufferLockBaseAddress(imageBuffer, 0);
-    
+
     // Get the number of bytes per row for the pixel buffer
     void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
-    
+
     // Get the number of bytes per row for the pixel buffer
     size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
     // Get the pixel buffer width and height
     size_t width = CVPixelBufferGetWidth(imageBuffer);
     size_t height = CVPixelBufferGetHeight(imageBuffer);
-    
+
     // Create a device-dependent RGB color space
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    
+
     // Create a bitmap graphics context with the sample buffer data
     CGContextRef context = CGBitmapContextCreate(baseAddress, width, height, 8,
                                                  bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
@@ -108,12 +102,16 @@
     CGImageRef quartzImage = CGBitmapContextCreateImage(context);
     // Unlock the pixel buffer
     CVPixelBufferUnlockBaseAddress(imageBuffer,0);
-    
+
     // Free up the context and color space
     CGContextRelease(context);
     CGColorSpaceRelease(colorSpace);
-    
-    return quartzImage;
+
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        CGImageRelease(self.lastCapturedImage);
+        self.lastCapturedImage = quartzImage;
+    });
+
 }
 
 - (void)startWithCallback:(ImageCallback)callback {
