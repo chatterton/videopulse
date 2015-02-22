050e0b5833eada6ab15a1f9aab28459c4df99f5c
Jack Chatterton <chattj@gmail.com>
2015-02-22 14:18:12 -0800

    crop by 50% to reduce extraneous capture

--- VideoPulse/VPStreamProcessor.m
+++ VideoPulse/VPStreamProcessor.m
@@ -13,6 +13,8 @@
     CIContext *ciContext;
 }
 
+const float FACE_CROP_FACTOR = 0.5;
+
 -(id)init {
     if ( self = [super init] ) {
         EAGLContext *eagl = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
@@ -31,7 +33,12 @@
 
     if (faceArray.count > 0) {
         CIFeature *face = faceArray[0];
-        CIImage *faceImage = [frame imageByCroppingToRect:CGRectMake(face.bounds.origin.x, face.bounds.origin.y, face.bounds.size.width, face.bounds.size.height)];
+        float croppedWidth = face.bounds.size.width * FACE_CROP_FACTOR;
+        float croppedHeight = face.bounds.size.height * FACE_CROP_FACTOR;
+        float croppedX = face.bounds.origin.x + ((face.bounds.size.width - croppedWidth) / 2.0);
+        float croppedY = face.bounds.origin.y + ((face.bounds.size.height - croppedHeight) / 2.0);
+        CIImage *faceImage = [frame imageByCroppingToRect:CGRectMake(croppedX, croppedY, croppedWidth, croppedHeight)];
+        NSLog(@"face of size %i, %i located at %i, %i", (int)croppedWidth, (int)croppedHeight, (int)croppedX, (int)croppedY);
         ref = [ciContext createCGImage:faceImage fromRect:[frame extent]];
     } else {
         NSLog(@"no face detected");
